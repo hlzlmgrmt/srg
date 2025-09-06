9f0348d65e9e1f0a7135affd522f620319077274
Peter Neuhoff <neuhoff.peter@gmx.de>
2025-09-06 16:37:38 +0200

    Auto-generated commit

---
+++ browser/chunk-KXXTFMZS.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-UR5WWB33.js";import"./chunk-RWYAEZUX.js";export{a as GmToolsModule};
---
+++ browser/chunk-RIIRJ3GO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-X724RBTZ.js";import"./chunk-RWYAEZUX.js";export{a as CharacterSheetModule};
