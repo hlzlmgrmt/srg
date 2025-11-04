6af641a12b9e191e7768daf4669ebf34f2722750
Peter Neuhoff <neuhoff.peter@gmx.de>
2025-11-04 20:59:01 +0100

    Auto-generated commit

---
+++ chunk-OJXOVCBM.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KNELBF4W.js";import"./chunk-CR6N3PST.js";export{a as CharacterSheetModule};
--- chunk-WBLDTDMG.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-Y6HDGGLX.js";import"./chunk-CR6N3PST.js";export{a as CharacterSheetModule};
