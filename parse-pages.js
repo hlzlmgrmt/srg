2e5a8999345e32f3eb0ab2886ae176c5fe3d1a7b
Peter Neuhoff <neuhoff.peter@gmx.de>
2025-12-02 18:30:11 +0100

    C

--- parse-pages.js
+++ parse-pages.js
@@ -7,9 +7,37 @@
 console.info('Running page content pre-parser');
 
 const fs = require('fs');
+const path = require('path');
+const walk = function(dir, done) {
+  let results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    let pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
 
 const destDir = './target/assets/pages';
 const createdDir = fs.mkdirSync(destDir, {recursive: true});
 createdDir && console.log('Created dir', createdDir);
+
+walk(__dirname + '/src/assets/pages', function (err, results) {
+  if (err) throw err;
+  console.log('Found ' + results.length + ' pages:', results);
+});
 
 console.info('Successfully parsed page content');
