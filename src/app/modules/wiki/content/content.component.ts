a6c848cb61ed09d7a93b4f16af36681604f686f5
Peter Neuhoff <neuhoff.peter@gmx.de>
2026-04-10 16:48:43 +0200

    Commit

---
+++ src/app/modules/wiki/content/content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'srg-content',
+  standalone: false,
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css',
+})
+export class ContentComponent {
+
+}
