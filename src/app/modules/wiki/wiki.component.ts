7c605aaa87bca1a45a6132e56aa3cdf8c3dc2593
Peter Neuhoff <neuhoff.peter@gmx.de>
2026-06-17 18:28:21 +0200

    Rewrite and refine basic matrix rules, add matrix equipment to wiki, add mask to content is navbar is showing and limit width to increase information density in wiki component

--- src/app/modules/wiki/wiki.component.ts
+++ src/app/modules/wiki/wiki.component.ts
@@ -39,6 +39,7 @@ interface NavigatableRoute {
 })
 export class WikiComponent {
   @ViewChild('navigation') navigation!: ElementRef<HTMLDivElement>;
+  @ViewChild('navigationMask') navigationMask!: ElementRef<HTMLDivElement>;
   @ViewChild('navigationToggler') toggler!: ElementRef<HTMLButtonElement>;
   @ViewChild('main') main!: ElementRef<HTMLDivElement>;
   @ViewChild('contentWrapper') contentWrapper!: ElementRef<HTMLDivElement>;
@@ -216,6 +217,8 @@ export class WikiComponent {
     this.navigation.nativeElement.classList.toggle('d-none');
     this.navigation.nativeElement.classList.toggle('d-flex');
     this.toggler.nativeElement.classList.toggle('d-block');
+    this.navigationMask.nativeElement.classList.toggle('d-none');
+    this.navigationMask.nativeElement.classList.toggle('d-block');
     this.main.nativeElement.classList.toggle('disabled');
   }
 
