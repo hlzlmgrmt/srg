31ec466293a9a2546e5963f93df8ef308f922f62
pheun <neuhoff.peter@gmx.de>
2026-06-18 18:48:56 +0200

    Auto-generated commit

---
+++ chunk-VHGB6Y2N.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-4WP525ID.js";import"./chunk-6YSIVWUO.js";export{a as CharacterSheetModule};
--- chunk-Y5EZVY3N.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-7TXNLSO7.js";import"./chunk-6YSIVWUO.js";export{a as CharacterSheetModule};
